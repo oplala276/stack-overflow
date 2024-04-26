9843fc20a38befed6d8e9b2c8b25a38578475e17
Oplala <oplala2020@gmail.com>
2024-04-26 22:30:24 +0530

    save changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+
 import AllRoutes from './AllRoutes';
 import {fetchAllUsers} from './actions/users'
 import { fetchAllQuestions } from './actions/question';
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   const dispatch = useDispatch();
