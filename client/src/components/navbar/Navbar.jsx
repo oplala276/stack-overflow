c207f4bfd6fbc138cae67950b9ac298ebff564a3
Oplala <oplala2020@gmail.com>
2024-04-27 07:35:45 +0530

    file name

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import './Navbar.css'
+import './navbar.css'
 import React, {useEffect} from 'react'
 import { useSelector, useDispatch} from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
